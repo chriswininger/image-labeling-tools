b3575383fdea68fb8b9dc0e602b906ac7bb60dbf
chris <chris.wininger@gmail.com>
2026-01-03 18:45:20 -0500

    possilby fixed the way images are being added to the context

--- quarkus-cli-image-labeler/src/main/java/com/wininger/cli_image_labeler/GenerateImageTagsCommand.java
+++ quarkus-cli-image-labeler/src/main/java/com/wininger/cli_image_labeler/GenerateImageTagsCommand.java
@@ -1,7 +1,5 @@
 package com.wininger.cli_image_labeler;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,7 +9,6 @@ import java.util.Base64;
 import com.wininger.cli_image_labeler.image.tagging.ImageInfo;
 import com.wininger.cli_image_labeler.image.tagging.ImageTagger;
 
-import dev.langchain4j.data.image.Image;
 import dev.langchain4j.data.message.ImageContent;
 import dev.langchain4j.model.chat.request.ResponseFormat;
 import dev.langchain4j.model.chat.request.ResponseFormatType;
@@ -44,21 +41,9 @@ public class GenerateImageTagsCommand implements Runnable {
             .chatModel(model)
             .build();
 
-        final Image image = getImage(imagePath);
-        final ImageInfo imageInfo = imageTagger.generateTags(image);
+        final ImageContent imageContent = getImageContent(imagePath);
+        final ImageInfo imageInfo = imageTagger.generateTags(imageContent);
         System.out.println("Image Info: " + imageInfo);
-    }
-
-    private String encodeInputStreamToBase64(final File file) {
-        try (final FileInputStream fis = new FileInputStream(file)) {
-            byte[] fileBytes = new byte[(int) file.length()];
-            fis.read(fileBytes); // Read bytes from the stream
-
-            // Encode the byte array to a Base64 string
-            return Base64.getEncoder().encodeToString(fileBytes);
-        } catch (IOException e) {
-            throw new RuntimeException("Coule not open the file: " + file.getName());
-        }
     }
 
     private String getMimeType(final String imagePath) {
@@ -74,16 +59,13 @@ public class GenerateImageTagsCommand implements Runnable {
         }
     }
 
-    private Image getImage(final String imagePath) {
+    private ImageContent getImageContent(final String imagePath) {
         try {
             final byte[] bytes = Files.readAllBytes(Paths.get(imagePath));
             final String base64Img = Base64.getEncoder().encodeToString(bytes);
             final String mimeType = getMimeType(imagePath);
 
-            return Image.builder()
-                .base64Data(base64Img)
-                .mimeType(mimeType)   // e.g. "image/png" or "image/jpeg"
-                .build();
+            return ImageContent.from(base64Img, mimeType);
         } catch(IOException ex) {
             throw new RuntimeException("Could not parse image: " + imagePath);
         }
